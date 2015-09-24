549bf7da97188b89ec22644f4257a33a7ad0a7fa
Adnan <adnankhan9890@gmail.com>
2015-09-24 17:27:07 +0530

    Recycler View added to the search activity so that it looks filled up and logical

--- app/src/main/java/tjs/co/in/groct/Search.java
+++ app/src/main/java/tjs/co/in/groct/Search.java
@@ -2,15 +2,31 @@ package tjs.co.in.groct;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class Search extends AppCompatActivity {
 
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    RecyclerView.Adapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        mRecyclerView = (RecyclerView) findViewById(R.id.Recycleview2);
+        mRecyclerView.setHasFixedSize(true);
+
+        // The number of Columns
+        mLayoutManager = new GridLayoutManager(this, 1);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new GridAdapter1();
+        mRecyclerView.setAdapter(mAdapter);
+
     }
 
     @Override
