a038e676f8130788ce8ee0efe0605d36271437e7
Adnan <adnankhan9890@gmail.com>
2015-09-24 16:10:54 +0530

    Search activity changed and some more minor changes

---
+++ app/src/main/java/tjs/co/in/groct/About_us.java
@@ -0,0 +1,17 @@
+package tjs.co.in.groct;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by Adnan Khan on 24-Sep-15.
+ */
+public class About_us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+
+}
---
+++ app/src/main/java/tjs/co/in/groct/My_wishlist.java
@@ -0,0 +1,17 @@
+package tjs.co.in.groct;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Adnan Khan on 24-Sep-15.
+ */
+public class My_wishlist extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_wishlist);
+    }
+}
